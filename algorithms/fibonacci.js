74ee31b9e772fcef13bd64433b59fbe3bc0587af
Michael Bruns <michaelbruns99@gmail.com>
2020-03-03 16:57:17 -0600

    Update fibonacci.js

--- algorithms/fibonacci.js
+++ algorithms/fibonacci.js
@@ -1,7 +1,20 @@
 //find the nth number of the fibonacci sequence
 
+// O(2^n)
+const fibonacciRecursive = (n) => {
+  let fibonacciList = [0, 1];
+  if (n === 0) {
+    return 0;
+  } else if (n === 1 || n === 2) {
+    return 1;
+  }
+
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+};
+
 // 0, 1, 1, 2, 3, 5, 8, 13, ...
 //n: 5 = 5
+// O(n)
 const fibonacciNthValueOf = (n) => {
   let fibonacciList = [0, 1];
   if (n === 0) {
